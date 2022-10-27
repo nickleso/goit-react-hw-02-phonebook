f6dc18b8690d9d22c53b502de1d07a7822bba79a
Nick Leso <nickleso.work@gmail.com>
2022-10-27 11:45:29 +0300

    Create contact form
    - create and render contact (names) form;
    - setState names on submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+// import { nanoid } from 'nanoid';
+// model.id = nanoid(10) //=> "V1StGXR8_Z5jdHi6B-myT"
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  // onInputChange = event => {
+  //   this.setState({ name: event.currentTarget.value });
+  // };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log('submit');
+    const form = event.currentTarget;
+    this.setState({ name: form.elements.name.value });
+    console.log(form.elements.name.value);
+    // this.props.onSubmit(this.state.name);
+
+    form.reset();
+    this.setState({ name: '' });
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.onFormSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <div>
+          <h2>Contacts</h2>
+
+          <ul></ul>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
